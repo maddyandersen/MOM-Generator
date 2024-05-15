7aa809fd813791df3f6447357a8987765e7811d5
Stella Oh <50852653+Stella-J-Oh@users.noreply.github.com>
2024-05-15 17:12:19 -0400

    categories and meals

--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -9,7 +9,7 @@ open Evaluator
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
+    member this.TestPrettyPrint () =
         let prog = "monday breakfast lee"
         let ast_maybe = parse prog
         match ast_maybe with
@@ -19,3 +19,26 @@ type TestClass () =
             Assert.AreEqual(expected, actual)
         | None ->
             Assert.IsTrue(false);
+
+    [<TestMethod>]
+    member this.TestReadFile () =
+        let prog = IO.File.ReadAllText("../../../../sample_prog/sample1.txt")
+        let ast_maybe = parse prog
+        match ast_maybe with
+        | Some ast ->
+            Assert.IsTrue(true)
+        | None ->
+            Assert.IsTrue(false);
+
+    (*
+    [<TestMethod>]
+    member this.TestGenerateCategory () =
+        let prog = IO.File.ReadAllText("../../../../sample_prog/invalid_order.txt")
+        let ast_maybe = parse prog
+        match ast_maybe with
+        | Some ast ->
+            Assert.IsTrue(true)
+        | None ->
+            Assert.IsTrue(false);
+    *)
+    
